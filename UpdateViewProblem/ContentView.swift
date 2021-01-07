b8b46e234ee0fd3ba96ab48408c9fc07ac9523c8
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-07 18:33:17 -0500

    Initial Commit

---
+++ UpdateViewProblem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UpdateViewProblem
+//
+//  Created by Iurie Guzun on 2021-01-07.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
