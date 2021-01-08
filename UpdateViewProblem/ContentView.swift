cd07e75a576960a404dff0d0ee17859093e95e0c
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-07 20:23:09 -0500

    All Done

--- UpdateViewProblem/ContentView.swift
+++ UpdateViewProblem/ContentView.swift
@@ -8,11 +8,45 @@
 
 import SwiftUI
 
+class Global: ObservableObject {
+    @Published var showMe = false
+   
+}
 struct ContentView: View {
+    @EnvironmentObject var global: Global
+//     @ObservedObject var global = Global()
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text((String(self.global.showMe)))
+           
+            if global.showMe {
+                Circle()
+                    .padding()
+            }
+            Button(action: {
+                self.global.showMe.toggle()
+             }) {
+                Text("Internal")
+                .foregroundColor(.red)
+                .padding()
+            }
+            ButtonView()
+        }
     }
 }
+
+struct ButtonView: View {
+    @ObservedObject var global = Global()
+    var body: some View {
+        Button(action: {
+            self.global.showMe.toggle()
+            print(self.global.showMe)
+        }) {
+        Text("External")
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
